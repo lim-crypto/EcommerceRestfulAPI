0d9acf299bc2b7f6108d03724924bda9359faede
lim <wamidulim@gmail.com>
2021-12-17 21:00:48 +0800

    changed name to word

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->word,
         'detail' => $faker->paragraph,
         'price' => $faker->numberBetween(100, 1000),
         'stock' => $faker->randomDigit,
